0aa8e3419f9d3902996d95616be70364c83984ad
Jingguo Yao <yaojingguo@gmail.com>
2019-09-01 14:45:10 +0800

    Add skeleton code from start.spring.io

---
+++ src/main/java/org/yao/curator/x/discovery/server/CuratorXDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package org.yao.curator.x.discovery.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuratorXDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuratorXDiscoveryServerApplication.class, args);
+	}
+
+}
